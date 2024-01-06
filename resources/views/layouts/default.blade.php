04191e7b4873678f0b99bc536732a4e2ced26332
Mo Shan <moshan2500@gmail.com>
2024-01-06 20:32:36 +0630

    Initial commit

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Tempo Ticket | @yield('title', $page_title ?? '')</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;400;700&display=swap" rel="stylesheet">
+
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/templatemo-festava-live.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    @include('elements.navbar')
+    
+    @yield('content')
+    
+    @include('elements.footer')
+
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.sticky.js') }}"></script>
+    <script src="{{ asset('js/click-scroll.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+</body>
+</html>
