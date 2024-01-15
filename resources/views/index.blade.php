779cf0edc48d2b9e49631ad016d160f2a9e55410
Mo Shan <moshan2500@gmail.com>
2024-01-16 00:59:57 +0630

    Revert "Added Migrations file and Breeze"
    This reverts commit 936273c75b26c875a658dd8124481a1514c6e05f.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.default')
-@section('title', 'Home Page')
 @section('content')
 <section class="hero-section" id="section_1">
     <div class="section-overlay"></div>
@@ -59,7 +58,7 @@
 
     <div class="video-wrap">
         <video autoplay="" loop="" muted="" class="custom-video" poster="">
-            <!-- <source src="video/pexels-2022395.mp4" type="video/mp4"> -->
+            <source src="video/pexels-2022395.mp4" type="video/mp4">
 
             Your browser does not support the video tag.
         </video>
