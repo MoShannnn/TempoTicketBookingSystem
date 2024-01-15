936273c75b26c875a658dd8124481a1514c6e05f
Mo Shan <moshan2500@gmail.com>
2024-01-15 23:52:53 +0630

    Added Migrations file and Breeze

---
+++ database/seeders/ConcertCategorySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ConcertCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
